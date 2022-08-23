8f92d321d934bca6e8cbbea1e2307661773608fc
gumphp <gumphp@qq.com>
2022-08-24 05:25:49 +0800

    init

---
+++ src/Providers/LaravelAdminAuthServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+namespace Gumphp\LaravelAdminAuth\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelAdminAuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
